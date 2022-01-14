64194fdabb24e915012a8006895db10363e1ec2b
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-14 15:34:39 +0100

    Created updateElements and got it working.
    Added updateElements to update numerical tags in the html. Removed testing stuff.

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const Bank = {
     balance: 0,
     loan: 0,
     getALoan: function(){
-        console.log("Test");
-        console.log(this.loan)
         if(this.loan > 0){
-            console.log("if")
             return;
         }
-        console.log("Test2")
         const loanAmount = prompt("How much money do you need to loan from the bank?");
+        if(loanAmount === NaN){
+            return;
+        }
         if(this.isLoanAmountAcceptable(loanAmount)){
             this.balance += parseInt(loanAmount);
             this.loan = loanAmount;
         }
+        updateElements();
     },
     isLoanAmountAcceptable(amount){
         if(parseInt(amount) > 2*this.balance){
@@ -31,6 +31,7 @@ const Bank = {
             this.loan -= this.balance;
             this.balance = 0;
         }
+        updateElements();
     },
     payOffLoanFromOutside(amount){
         if(amount > this.loan){
@@ -41,27 +42,37 @@ const Bank = {
         else{
             this.loan -= amount;
         }
+        updateElements();
     }
 };
 let Work = {
     pay: 0,
     doTheWork: function(){
-        console.log(this.pay);
         this.pay += 100;
-        console.log(this.pay);
-        const newPayElement = document.getElementById("pay").innerText = this.pay;
+        updateElements();
     },
     putPayInBank: function(){
         if(Bank.loan > 0){
-            Bank.payOffLoanPartiallyWithPay(0.1*this.pay);
+            Bank.payOffLoanFromOutside(0.1*this.pay);
             Bank.balance += 0.9*this.pay;
             this.pay = 0;
         }
+        else{
+            Bank.balance += this.pay;
+            this.pay = 0;
+        }
+        updateElements();
     },
     payOffLoanWithPay: function(){
         Bank.payOffLoanFromOutside(this.pay);
         this.pay = 0;
+        updateElements();
     }
+};
+function updateElements(){
+    payElement = document.getElementById("pay").innerText = Work.pay;
+    loanElement = document.getElementById("loan").innerText = Bank.loan;
+    balanceElement = document.getElementById("balance").innerText = Bank.balance;
 };
 const laptopsElement = document.getElementById("laptops");
 const priceElement = document.getElementById("price");
@@ -70,9 +81,9 @@ const workElement = document.getElementById("work");
 const payOffLoanElement = document.getElementById("payOff");
 const putPayInBankElement = document.getElementById("putPayInBank");
 const payOffLoanWithPayElement = document.getElementById("payOffLoanWithPay");
-const balanceElement = document.getElementById("balance").innerText = Bank.balance;
-const loanElement = document.getElementById("loan").innerText = Bank.loan;
-const payElement = document.getElementById("pay").innerText = Work.pay;
+let balanceElement = document.getElementById("balance").innerText = Bank.balance;
+let loanElement = document.getElementById("loan").innerText = Bank.loan;
+let payElement = document.getElementById("pay").innerText = Work.pay;
 
 let laptops = [];
 fetch("https://noroff-komputer-store-api.herokuapp.com/computers").then(response => response.json()).then(data => laptops = data).then(laptops => addLaptopsToList(laptops));
