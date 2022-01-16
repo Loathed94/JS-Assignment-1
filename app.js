b8f29086a15f734940b141a5943a1967f6b27c1b
Christian Neij <82954891+Loathed94@users.noreply.github.com>
2022-01-16 12:42:24 +0100

    Documentation.
    Comments, comments everywhere!

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const Bank = {
-    balance: 0,
-    loan: 0,
-    getALoan: function(){
-        if(this.loan > 0){
+const Bank = { //Object representing the bank with member attributes and methods.
+    balance: 0, //Customer's balance.
+    loan: 0, //Debt, money borrowed goes into balance, this shows debt.
+    getALoan: function(){ //The method called when user wishes to get a loan.
+        if(this.loan > 0){ //If user has debt no new loan is allowed, so method returns without doing anything.
             return;
         }
-        const loanAmount = prompt("How much money do you need to loan from the bank?");
-        if(loanAmount === NaN){
+        const loanAmount = prompt("How much money do you need to loan from the bank?"); //Asks user to input how much they wish to borrow.
+        if(loanAmount === NaN){ //If input is cancelled the method returns without doing any more work.
             return;
         }
-        if(this.isLoanAmountAcceptable(loanAmount)){
-            this.balance += parseInt(loanAmount);
+        if(this.isLoanAmountAcceptable(loanAmount)){ //A different method is called that makes sure the loan amount is not too much.
+            this.balance += parseInt(loanAmount); //If loan amount is acceptable then the loan is granted.
             this.loan = parseInt(loanAmount);
         }
-        updateElements();
+        updateElements(); //HTML is updated when numbers are changed.
     },
-    isLoanAmountAcceptable(amount){
+    isLoanAmountAcceptable(amount){ //Method checks if loan amount are acceptable.
         if(parseInt(amount) > 2*this.balance){
             return false;
         }
         return true;
     },
-    payOffLoanWithBalance(){
-        if(this.balance >= this.loan){
+    payOffLoanWithBalance(){ //Method uses balance in bank to pay off loans. 
+        if(this.balance >= this.loan){ //If balance is greater than debt the debt is zero'd and balance is reduced by debt size.
             this.balance -= this.loan;
             this.loan = 0;
         }
-        else{
+        else{ //If balance is smaller than debt then balance is zero'd and debt is decreased by balance size.
             this.loan -= this.balance;
             this.balance = 0;
         }
         updateElements();
     },
-    payOffLoanFromOutside(amount){
+    payOffLoanFromOutside(amount){ //Similar to method above but allows un-specified amounts of cash from outside to pay off (entirely or partially) the debt.
         if(amount > this.loan){
             let residual = amount - this.loan;
             this.loan = 0;
-            this.balance += residual;
+            this.balance += residual; //If money put towards decreasing debt is greater than the debt the rest of the money goes to balance.
         }
         else{
             this.loan -= amount;
@@ -44,42 +44,42 @@ const Bank = {
         updateElements();
     }
 };
-const Work = {
-    pay: 0,
-    doTheWork: function(){
+const Work = { //An object representing the workplace with its members.
+    pay: 0, //Accumulated salary.
+    doTheWork: function(){ //Method for performing work and generating pay. 
         this.pay += 100;
         updateElements();
     },
-    putPayInBank: function(){
-        if(Bank.loan > 0){
+    putPayInBank: function(){ //Puts money in pay into the bank.
+        if(Bank.loan > 0){ //If worker is in debt 10% goes to clearing the debt, 90% goes to balance.
             Bank.payOffLoanFromOutside(0.1*this.pay);
             Bank.balance += 0.9*this.pay;
             this.pay = 0;
         }
-        else{
+        else{ //If there is no debt all the cash goes to balance.
             Bank.balance += this.pay;
             this.pay = 0;
         }
         updateElements();
     },
-    payOffLoanWithPay: function(){
+    payOffLoanWithPay: function(){ //This method uses all the money in pay towards clearing debt, any remaining cash goes to balance.
         Bank.payOffLoanFromOutside(this.pay);
         this.pay = 0;
         updateElements();
     }
 };
-function buy(){
+function buy(){ //A function that allows the user to buy a laptop of their choice! (whichever laptop is selected at the time.)
     const price = parseInt(priceElement.innerText);
-    if(price <= Bank.balance){
+    if(price <= Bank.balance){ //...assuming they can afford it.
         Bank.balance -= price;
     }
     updateElements();
 }
-function updateElements(){
+function updateElements(){ //Any time a user performs an action that can change what is displayed on the webpage this function makes sure to keep the html updated.
     payElement.innerText = Work.pay;
-    loanElement.innerText = Bank.loan;
+    loanElement.innerText = Bank.loan; //Update numbers
     balanceElement.innerText = Bank.balance;
-    if(Bank.loan > 0){
+    if(Bank.loan > 0){ //Hide or unhide buttons
         payOffLoanElement.style.display = "block";
         payOffLoanWithPayElement.style.display = "block";
     }
@@ -91,64 +91,67 @@ function updateElements(){
 const laptopsElement = document.getElementById("laptops");
 const priceElement = document.getElementById("price");
 const specsElement = document.getElementById("specs");
-const descriptionElement = document.getElementById("description");
+const descriptionElement = document.getElementById("description"); //A bunch of elements corresponding to items in the html.
 const getALoanElement = document.getElementById("getLoan");
 const workElement = document.getElementById("work");
 const payOffLoanElement = document.getElementById("payOff");
-payOffLoanElement.style.display ="none";
+payOffLoanElement.style.display ="none"; //Hidden by default.
 const putPayInBankElement = document.getElementById("putPayInBank");
 const payOffLoanWithPayElement = document.getElementById("payOffLoanWithPay");
 payOffLoanWithPayElement.style.display = "none";
 const balanceElement = document.getElementById("balance");
 balanceElement.innerText = Bank.balance;
 const loanElement = document.getElementById("loan");
-loanElement.innerText = Bank.loan;
+loanElement.innerText = Bank.loan; //Default values printed.
 const payElement = document.getElementById("pay");
 payElement.innerText = Work.pay;
 const buyElement = document.getElementById("buy");
-let currentSpecs = [];
+let currentSpecs = []; //An array storing specs currently displayed in the html. Used to keep track of which specs are used before new ones take over to make it easier for the program to remove old childNode-specs.
 
-let laptops = [];
-fetch("https://noroff-komputer-store-api.herokuapp.com/computers").then(response => response.json()).then(data => laptops = data).then(laptops => addLaptopsToList(laptops));
+let laptops = []; //The array storing the laptop-objects from the .json fetch.
+fetch("https://noroff-komputer-store-api.herokuapp.com/computers") //Fetch the API-promise.
+.then(response => response.json()) //Extract .json from Promise
+.then(data => laptops = data) //Store data from .json in laptops array.
+.then(laptops => addLaptopsToList(laptops)); //Send laptops array to a function shown below.
 
-function addLaptopsToList(laptops){
-    for (const laptop of laptops) {
-        const laptopElement = document.createElement("option");
-        laptopElement.value = laptop.id;
-        laptopElement.appendChild(document.createTextNode(laptop.title));
-        laptopsElement.appendChild(laptopElement);
+function addLaptopsToList(laptops){ //Laptops are sent to this function.
+    for (const laptop of laptops) { //Laptops are looped over, selected one by one.
+        const laptopElement = document.createElement("option"); //For each laptop an element is created with "option" type chosen for html-select.
+        laptopElement.value = laptop.id; //value of element set to laptop's unique id.
+        laptopElement.appendChild(document.createTextNode(laptop.title)); //Name of laptop added to element as a childNode.
+        laptopsElement.appendChild(laptopElement); //laptop element added to greater element of all laptops (notice slight difference in name)
     }
-    priceElement.innerText = laptops[0].price;
-    descriptionElement.innerText = laptops[0].description;
-    for (const spec of laptops[0].specs) {
+    priceElement.innerText = laptops[0].price; 
+    descriptionElement.innerText = laptops[0].description; //Default price and description added to html at start.
+    for (const spec of laptops[0].specs) { //Same thing for specs, but as unspecified amount of list items
         addSpecToHTML(spec);
     }
 }
-function handleLaptopListChange(e){
-    const selectedLaptop = laptops[e.target.selectedIndex];
-    priceElement.innerText = selectedLaptop.price;
+function handleLaptopListChange(e){ //When a laptop is selected on the webpage it is updated with new laptop information.
+    const selectedLaptop = laptops[e.target.selectedIndex]; //Event shows which laptop was selected.
+    priceElement.innerText = selectedLaptop.price; //Price and description is changed to that of new laptop.
     descriptionElement.innerText = selectedLaptop.description;
-    for (const child of currentSpecs) {
+    for (const child of currentSpecs) { //Old specifications from previous laptop that were added as children are removed.
         specsElement.removeChild(child);
     }
-    currentSpecs = [];
-    const specArray = selectedLaptop.specs;
-    for (const spec of specArray) {
+    currentSpecs = []; //CurrentSpecs is reset to empty array.
+    const specArray = selectedLaptop.specs; //New specs found.
+    for (const spec of specArray) { //New specs looped over.
         addSpecToHTML(spec);
     }
 }
-function addSpecToHTML(spec){
-    const currentSpec = document.createElement("li");
-    currentSpec.innerText = spec; //??
-    currentSpecs.push(currentSpec);
-    specsElement.appendChild(currentSpec);
+function addSpecToHTML(spec){ //Whenever specs are added to html it is done here.
+    const currentSpec = document.createElement("li"); //list item created.
+    currentSpec.innerText = spec; //spec added as text to li.
+    currentSpecs.push(currentSpec); //New spec added to list of specs.
+    specsElement.appendChild(currentSpec); //New spec added as child to html-element of specs.
 }
  
 
-laptopsElement.addEventListener("change", handleLaptopListChange);
-workElement.addEventListener("click", Work.doTheWork.bind(Work));
-payOffLoanElement.addEventListener("click", Bank.payOffLoanWithBalance.bind(Bank));
-putPayInBankElement.addEventListener("click", Work.putPayInBank.bind(Work));
-getALoanElement.addEventListener("click", Bank.getALoan.bind(Bank));
-payOffLoanWithPayElement.addEventListener("click", Work.payOffLoanWithPay.bind(Work));
-buyElement.addEventListener("click", buy);
+laptopsElement.addEventListener("change", handleLaptopListChange); //Listener that notices change of selected laptop and fires relevant function.
+workElement.addEventListener("click", Work.doTheWork.bind(Work)); //Listener that notices Work-button click and fires relevant method.
+payOffLoanElement.addEventListener("click", Bank.payOffLoanWithBalance.bind(Bank)); //Listener that notices Pay-off-loan-button click and fires relevant method.
+putPayInBankElement.addEventListener("click", Work.putPayInBank.bind(Work)); //Listener that notices Deposit-pay-button click and fires relevant method.
+getALoanElement.addEventListener("click", Bank.getALoan.bind(Bank)); //Listener that notices Get-a-loan-button click and fires relevant method.
+payOffLoanWithPayElement.addEventListener("click", Work.payOffLoanWithPay.bind(Work)); //Listener that notices pay-off-loan-with-pay-button click and fires relevant method.
+buyElement.addEventListener("click", buy); //Listener that notices Buy-button click and fires relevant function.
